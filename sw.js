d5c4f0f0456676c63bec81d34d747f197cdf26d7
deguyts-rgb <deguyts@gmail.com>
2026-03-17 21:27:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = ‘ani-beseder-v73’;
+var CACHE_NAME = “ani-beseder-v73”;
 
-self.addEventListener(‘install’, function(e) {
+self.addEventListener(“install”, function(e) {
 e.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener(‘activate’, function(e) {
+self.addEventListener(“activate”, function(e) {
 e.waitUntil(
 caches.keys()
 .then(function(keys) {
@@ -15,41 +15,41 @@ keys.filter(function(k) { return k !== CACHE_NAME; })
 })
 .then(function() { return self.clients.claim(); })
 .then(function() {
-return self.clients.matchAll({ type: ‘window’, includeUncontrolled: true });
+return self.clients.matchAll({ type: “window”, includeUncontrolled: true });
 })
 .then(function(clients) {
-clients.forEach(function(c) { c.postMessage({ type: ‘SW_UPDATED’ }); });
+clients.forEach(function(c) { c.postMessage({ type: “SW_UPDATED” }); });
 })
 );
 });
 
-self.addEventListener(‘fetch’, function(e) {});
+self.addEventListener(“fetch”, function(e) {});
 
-self.addEventListener(‘push’, function(e) {
+self.addEventListener(“push”, function(e) {
 if (!e.data) return;
 var data;
-try { data = e.data.json(); } catch(err) { data = { notification: { title: ‘ani beseder’, body: e.data.text() } }; }
+try { data = e.data.json(); } catch(err) { data = { notification: { title: “ani beseder”, body: e.data.text() } }; }
 var n = data.notification || {};
-e.waitUntil(self.registration.showNotification(n.title || ‘ani beseder’, {
-body: n.body || ‘’,
-icon: ‘/icon-192.png’,
-badge: ‘/icon-192.png’,
-dir: ‘rtl’,
-lang: ‘he’,
+e.waitUntil(self.registration.showNotification(n.title || “ani beseder”, {
+body: n.body || “”,
+icon: “/icon-192.png”,
+badge: “/icon-192.png”,
+dir: “rtl”,
+lang: “he”,
 requireInteraction: true,
 vibrate: [200, 100, 200],
-data: { url: n.url || ‘https://anibeseder.com’ }
+data: { url: n.url || “https://anibeseder.com” }
 }));
 });
 
-self.addEventListener(‘notificationclick’, function(e) {
+self.addEventListener(“notificationclick”, function(e) {
 e.notification.close();
-var targetUrl = (e.notification.data && e.notification.data.url) ? e.notification.data.url : ‘https://anibeseder.com’;
+var targetUrl = (e.notification.data && e.notification.data.url) ? e.notification.data.url : “https://anibeseder.com”;
 e.waitUntil(
-clients.matchAll({ type: ‘window’, includeUncontrolled: true })
+clients.matchAll({ type: “window”, includeUncontrolled: true })
 .then(function(list) {
 for (var i = 0; i < list.length; i++) {
-if (list[i].url.includes(‘anibeseder’) && ‘focus’ in list[i]) return list[i].focus();
+if (list[i].url.indexOf(“anibeseder”) > -1 && “focus” in list[i]) return list[i].focus();
 }
 return clients.openWindow(targetUrl);
 })
